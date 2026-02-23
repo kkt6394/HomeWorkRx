b77266820b300bea30626a5f2474ab472aa2ac5c
kkt6394 <kkt6394@gmail.com>
2026-02-23 16:25:12 +0900

    Initial Commit

---
+++ HomeWorkRx/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWorkRx
+//
+//  Created by 김기태 on 2/23/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
